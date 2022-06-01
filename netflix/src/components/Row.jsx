bac38282b0669f4021075a7954e20ccec1395075
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-06-01 14:14:42 +0400

    home page done!!!

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from 'react';
 import {MdChevronLeft, MdChevronRight} from 'react-icons/md';
 import Movie from './Movie';
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
     const [movies, setMovies] = useState([]);
 
 
@@ -18,22 +18,38 @@ const Row = ({ title, fetchURL }) => {
         
     },[fetchURL])
 
-    console.log(movies);
+    const slideLeft = () => {
+        var slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+
+    const slideRight = () => {
+        var slider = document.getElementById('slider' + rowID);
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
 
     return (
         <div>
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
             <div className='relative flex items-center group'>
-                <MdChevronLeft size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
-                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+                <MdChevronLeft 
+                    onClick={slideLeft}
+                    size={40} 
+                    className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+                />
+                <div id={'slider' + rowID} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.map((item, id) => (
                         <Movie key={id} item={item}/>
                     ))}
                 </div>
-                <MdChevronRight size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
+                <MdChevronRight 
+                    onClick={slideRight}
+                    size={40} 
+                    className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+                />
             </div>
         </div>
     )
 }
 
-export default Row;
+export default Row;;
