a701c0693c27b98b4bdceae260de89492ebdb7a5
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-06-01 13:52:11 +0400

    movie eroor fixed

--- netflix/src/components/Row.jsx
+++ netflix/src/components/Row.jsx
@@ -24,13 +24,13 @@ const Row = ({ title, fetchURL }) => {
         <div>
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
             <div className='relative flex items-center group'>
-                <MdChevronLeft size={40} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
-                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative hidden group-hover:block'>
+                <MdChevronLeft size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
+                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.map((item, id) => (
                         <Movie key={id} item={item}/>
                     ))}
                 </div>
-                <MdChevronRight size={40} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden'/>
+                <MdChevronRight size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
             </div>
         </div>
     )
